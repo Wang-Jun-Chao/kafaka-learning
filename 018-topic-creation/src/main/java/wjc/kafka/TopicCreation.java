8468900cfd44ef322cc254fc666b928018e9447b
王俊超 <wangjunchao@gf.com.cn>
2018-10-18 20:04:39 +0800

    commit

---
+++ 018-topic-creation/src/main/java/wjc/kafka/TopicCreation.java
@@ -0,0 +1,61 @@
+package wjc.kafka;
+
+import kafka.admin.AdminUtils;
+import kafka.admin.RackAwareMode;
+import kafka.utils.ZkUtils;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.common.security.JaasUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.UUID;
+
+
+/**
+ * @author: wangjunchao(王俊超)
+ * @time: 2018-10-18 19:13
+ **/
+public class TopicCreation {
+    private final static Logger logger          = LoggerFactory.getLogger(TopicCreation.class);
+    /**
+     * 连接ZK
+     */
+    private static final String ZK_CONNECT      = "10.1.177.73:2181";
+    /**
+     * session 过期时间
+     */
+    private static final int    SESSION_TIMEOUT = 30000;
+    /**
+     * 连接超时时间
+     */
+    private static final int    CONNECT_TIMEOUT = 30000;
+
+
+    public static void createTopic(String topic, int partition, int replicate, Properties properties) {
+        ZkUtils zkUtils = null;
+
+        try {
+            // 实例化ZkUtils
+            zkUtils = ZkUtils.apply(ZK_CONNECT, SESSION_TIMEOUT, CONNECT_TIMEOUT,
+                    JaasUtils.isZkSecurityEnabled());
+            if (AdminUtils.topicExists(zkUtils, topic)) {
+                AdminUtils.createTopic(zkUtils, topic, partition, replicate, properties,null);
+
+                logger.warn("the topic [" + topic + "] has been created");
+            } else {
+                logger.warn("the topic [" + topic + "] is already exist");
+                AdminUtils.deleteTopic(zkUtils, topic);
+                logger.warn("the topic [" + topic + "] has been deleted");
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        } finally {
+            zkUtils.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        createTopic(UUID.randomUUID().toString().replace("-", ""), 1, 1, new Properties());
+    }
+}
